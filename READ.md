639dad4b21e61043b25ac9ec29ea06d8b0ef9204
julia goulart <socialmjuliagoulart@gmail.com>
2024-04-05 14:44:12 -0300

    first commit

--- READ.md
+++ READ.md
@@ -1,1 +1,46 @@
-# spotify-imersao
+# Projeto Inicial
+Desafio proposto pela Alura para testar lógica de programação com JavaScript. O projeto se baseia em criar um jogo com tentativas para acertar um número secreto, com condições, acertos e erros.
+
+## Índice
+- <a href= "#funcionalidades">Funcionalidades do Projeto</a>
+- <a href= "#tecnologias utilizadas">Tecnologias utilizadas</a>
+- <a href= "#rodar">Como rodar este projeto?</a>
+- <a href= "#pessoa">Pessoa autora</a>
+
+## 📱Funcionalidades do projeto
+
+- Oferecer um alert para que o usuário adivinhe o número secreto;
+![home](./img/home.png)
+- Validar o valor digitado se o usuário acertou ou errou;
+![tentativa1](./img/tentativa1.png)
+- Tela de acerto para o cenário de sucesso e quantidade de tentativas;
+![tentativa3](./img/tentativa3.png)
+- Possibilidade de nova tentativa em cenário de erro.
+![tentativa2](./img/tentativa2.png)
+
+## Tecnologias utilizadas
+
+1. [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+2. [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+3. [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## 💻Como rodar este projeto?
+```bash
+# Clone este repositório
+$ git clone linkrepo
+
+# Acesse a pasta do projeto no seu terminal
+$ logica-js-projeto_inicial
+
+# Instale as dependências
+$ npm install
+
+# Execute a aplicação
+$ npm run start
+
+# A aplicação será iniciada na porta 3000, acesse pelo navegador: http://localhost:3000
+```
+
+## Pessoa autora
+
+[Julia Goulart - Linkedin](https://www.linkedin.com/in/juliagoulartux/)
--- search.js
+++ search.js
@@ -3,38 +3,22 @@ const playlistContainer = document.getElementById("result-playlists");
 const searchInput = document.getElementById("search-input");
 
 function requestApi(searchTerm) {
-  fetch(`http://localhost:3000/artists`)
-    .then((response) => response.json()) 
-    // results -> [{name: "Foo Fighters", age: 20}, {name: "Emicida"}]
-    // results -> filter -> Uma função que me passa cada valor do result, um a um com o nome que eu dei
-    .then((results) => results.filter((item) => item.name.toLowerCase().includes(searchTerm)))
-    .then((filteredResults) => displayResults(filteredResults));
+  const url = `http://localhost:3000/artists?name_like=f${searchTerm}`
+    fetch(url)
+    .then((response) => response.json())
+    .then((results) => displayResults(results));
 }
 
 function displayResults(results) {
   hidePlaylists();
   const artistImage = document.getElementById("artist-img");
   const artistName = document.getElementById("artist-name");
+  console.log(displayResults)
 
-  console.log({results});
-
-  // Três formas de fazer a mesma coisa
-  // for (const barbie of results) {
-  //   artistImage.src = barbie.urlImg;
-  //   artistName.innerText = barbie.name;
-  // }
-
-  for (let i = 0; i < results.length; i++) {
-    const barbie = results[i];
-
-    artistImage.src = barbie.urlImg;
-    artistName.innerText = barbie.name;
-  }
-
-  // results.forEach((barbie) => {
-  //   artistImage.src = barbie.urlImg;
-  //   artistName.innerText = barbie.name;
-  // });
+  results.forEach((element) => {
+    artistImage.src = element.urlImg;
+    artistName.innerText = element.name;
+  });
   resultArtist.classList.remove("hidden");
 }
 
